448e26f27ff03410ba1d4fec88208315f5d50841
Gustavo Bruno <gugubrunopf@gmail.com>
2025-05-04 23:34:07 -0300

    new pokedex

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+
+
+export function Home() {
+  return (
+    <div>
+        <Navbar />
+        <h1>Pokedex</h1>
+        <Footer />
+    </div>
+  );
+}
+
+export default Home;
